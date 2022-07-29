bbf86bbfa94cc59590b06df7da0593869d2fdbb7
Bruno Bispo <brunobispoo25@gmail.com>
2022-07-28 22:04:51 -0300

    Foram criadas as tabelas de produtos e clientes.

--- database/tableCreation.sql
+++ database/tableCreation.sql
@@ -17,3 +17,28 @@ CREATE TABLE categorias(
 	id SERIAL PRIMARY KEY,
   descricao TEXT NOT NULL
 );
+
+DROP TABLE IF EXISTS produtos;
+
+CREATE TABLE produtos(
+	id SERIAL PRIMARY KEY,
+  descricao TEXT NOT NULL,
+  quantidade_estoque INTEGER NOT NULL,
+  valor INTEGER NOT NULL,
+  categoria_id INTEGER REFERENCES categorias(id)
+);
+
+DROP TABLE IF EXISTS clientes;
+
+CREATE TABLE clientes(
+	id SERIAL PRIMARY KEY,
+  nome VARCHAR(50) NOT NULL,
+  email TEXT NOT NULL UNIQUE,
+  cpf CHAR(11) NOT NULL UNIQUE,
+  cep CHAR(8),
+  rua TEXT,
+  numero SMALLINT,
+  bairro TEXT,
+  cidade TEXT,
+  estado CHAR(2)
+);
