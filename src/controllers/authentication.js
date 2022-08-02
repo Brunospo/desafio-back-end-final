7e546ac985b3b05062093e3572f49bf3ebb8a6dd
Bruno Bispo <brunobispoo25@gmail.com>
2022-08-02 20:08:51 -0300

    Removendo os try e catch da rota de login.

--- src/controllers/authentication.js
+++ src/controllers/authentication.js
@@ -1,18 +1,11 @@
-/* eslint-disable */
-
 const jwt = require('jsonwebtoken');
 
 const loginUser = async (req, res) => {
 	const user = req.user;
 
-	try {
-		const token = jwt.sign(user, process.env.JWT_SECUREPASSWORD, { expiresIn: '2h' });
+	const token = jwt.sign(user, process.env.JWT_SECUREPASSWORD, { expiresIn: '2h' }); //eslint-disable-line
 
-		return res.json({ usuario: { ...user }, token })
-
-	} catch (error) {
-		return res.status(500).json({ message: error.message });
-	}
+	return res.json({ usuario: { ...user }, token });
 };
 
 module.exports = {
--- src/middlewares/authenticationMiddleware.js
+++ src/middlewares/authenticationMiddleware.js
@@ -1,34 +1,26 @@
 const knex = require('../config/knexConnection');
 const validateAuthenticationFields = require('../schemas/yupAuthenticationSchema');
+const { BadRequestError } = require('../utils/apiErros');
 const { isCorrectPassword } = require('../utils/bcrypt');
 
 const validateBodyAuthentication = async (req, res, next) => {
 	const { email, senha } = req.body;
 
-	try {
-		await validateAuthenticationFields.validate({ email, senha });
+	await validateAuthenticationFields.validate({ email, senha });
 
-		const user = await knex('usuarios').where({ email }).first();
+	const user = await knex('usuarios').where({ email }).first();
 
-		if (!user) {
-			return res.status(400).json({ message: 'Email e/ou senha inválido(s).' });
-		}
-
-		const correctPassword = await isCorrectPassword(senha, user.senha);
-
-		if (!correctPassword) {
-			return res.status(400).json({ message: 'Email e/ou senha inválido(s).' });
-		}
-
-		req.user = { id: user.id, nome: user.nome, email: user.email };
-
-	} catch (error) {
-		if (error.name === 'ValidationError'){
-			return res.status(400).json({ message: error.message });
-		}
-		
-		return res.status(500).json({ message: error.message });
+	if (!user) {
+		throw new BadRequestError('Email e/ou senha inválido(s).');
 	}
+
+	const correctPassword = await isCorrectPassword(senha, user.senha);
+
+	if (!correctPassword) {
+		throw new BadRequestError('Email e/ou senha inválido(s).');
+	}
+
+	req.user = { id: user.id, nome: user.nome, email: user.email };
 
 	next();
 };
