0f0e3fb5fb2bcc3d0c462f6f607107475db4b779
Bruno Bispo <brunobispoo25@gmail.com>
2022-08-10 22:51:12 -0300

    Melhorado o retorno do yup do pedido_produtos.

--- src/schemas/yupOrderSchema.js
+++ src/schemas/yupOrderSchema.js
@@ -4,8 +4,8 @@ const validateOrderFields = yup.object().shape({
 	cliente_id: yup.number().integer('cliente_id deve receber um número inteiro').positive().required(),
 	observacao: yup.string(),
 	pedido_produtos: yup.array().of(yup.object().shape({
-		produto_id: yup.number().positive().integer('produto_id deve receber um número inteiro').required('produto_id é um campo obrigatório'),
-		quantidade_produto: yup.number().positive().integer('quantidade_produto deve receber um número inteiro').required('quantidade_produto é um campo obrigatório')
+		produto_id: yup.number().positive().integer().required(),
+		quantidade_produto: yup.number().positive().integer().required()
 	})).min(1, 'pedido_produtos deve ter pelo menos 1 item').required()
 });
 
