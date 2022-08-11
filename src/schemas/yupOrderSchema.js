efdbb23fc0325b386a6770a56991e11cbf670875
Bruno Bispo <brunobispoo25@gmail.com>
2022-08-10 22:41:44 -0300

    Adicionado o required no yup do pedido_produtos.

--- src/schemas/yupOrderSchema.js
+++ src/schemas/yupOrderSchema.js
@@ -6,7 +6,7 @@ const validateOrderFields = yup.object().shape({
 	pedido_produtos: yup.array().of(yup.object().shape({
 		produto_id: yup.number().positive().integer('produto_id deve receber um número inteiro').required('produto_id é um campo obrigatório'),
 		quantidade_produto: yup.number().positive().integer('quantidade_produto deve receber um número inteiro').required('quantidade_produto é um campo obrigatório')
-	})).min(1, 'pedido_produtos deve ter pelo menos 1 item')
+	})).min(1, 'pedido_produtos deve ter pelo menos 1 item').required()
 });
 
 const validateQueryId = yup.object().shape({
